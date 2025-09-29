bc11435f30d950042cfbf7f6b0f20bf028298a6d
Fachri Hawari <fachri.hawari@gmail.com>
2025-09-29 22:12:01 +0700

    feat: add axios for HTTP requests and create http instance; update HomePage to fetch projects

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,15 @@
 import { Button } from "@/components/ui/button"
+import { http } from "@/libs/http"
+import { useEffect } from "react"
 import { Link } from "react-router"
 
 export function HomePage() {
+
+  useEffect(() => {
+    http.get('/projects')
+      .then(console.log)
+  }, [])
+
   return (
     <div>
       Home Page
