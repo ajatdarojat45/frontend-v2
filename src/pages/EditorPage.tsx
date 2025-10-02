554d5205a6bfd0f2453bbea4767daaf335954eb0
Fachri Hawari <fachri.hawari@gmail.com>
2025-10-02 10:06:50 +0700

    feat: implement SimulationPicker component and integrate active simulation management in EditorPage

--- src/pages/EditorPage.tsx
+++ src/pages/EditorPage.tsx
@@ -1,23 +1,37 @@
 import { CreateSimulation } from "@/components/features/CreateSimulation";
 import { EmptySimulation } from "@/components/features/EmptySimulation";
+import { SimulationPicker } from "@/components/features/SimulationPicker";
 import { AppLayout } from "@/components/ui/app-layout";
 import { useGetSimulationsByModelIdQuery } from "@/store/simulationApi";
+import { setActiveSimulation } from "@/store/simulationSlice";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useNavigate, useParams } from "react-router";
 
 export function EditorPage() {
   const navigate = useNavigate();
   const { modelId, simulationId } = useParams() as { modelId: string; simulationId?: string };
   const { data: simulations } = useGetSimulationsByModelIdQuery(+modelId);
+  const dispatch = useDispatch();
 
   // If no simulationId is provided, redirect to the first simulation
   // Once the simulations are created, the effect will run again
   useEffect(() => {
-    if (simulations && simulations.length > 0 && !simulationId) {
-      const firstSimulationId = simulations[0].id;
-      navigate(`/editor/${modelId}/${firstSimulationId}`, { replace: true });
+    if (simulations && simulations.length > 0) {
+      // If simulationId is provided, find the active simulation and set it in the store
+      if (simulationId) {
+        const activeSimulation = simulations.find((sim) => sim.id === +simulationId);
+        if (activeSimulation) {
+          dispatch(setActiveSimulation(activeSimulation));
+          return;
+        }
+      }
+
+      // If no simulationId or invalid simulationId, redirect to the first simulation
+      const firstSimulation = simulations[0];
+      dispatch(setActiveSimulation(firstSimulation));
     }
-  }, [simulations, simulationId, modelId, navigate]);
+  }, [simulations, simulationId, modelId, navigate, dispatch]);
 
   return (
     <AppLayout
@@ -27,7 +41,8 @@ export function EditorPage() {
           {
             // If there's a simulationId, show the simulation editor
             simulationId ? (
-              <div className="w-full h-full flex flex-col">
+              <div className="w-full h-full flex flex-col p-4">
+                <SimulationPicker modelId={+modelId} simulationId={+simulationId} />
                 You're editing simulation {simulationId}
               </div>
             ) : (
