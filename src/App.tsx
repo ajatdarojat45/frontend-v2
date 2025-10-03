48223802b7d4b6a3edb033f7fb35ac6cd3a7cd21
Fachri Hawari <fachri.hawari@gmail.com>
2025-10-03 11:30:36 +0700

    feat: Handle not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Provider as ReduxProvider } from "react-redux";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { Navigate, BrowserRouter, Route, Routes } from "react-router";
 import { Toaster } from "@/components/ui/sonner";
 import { HomePage } from "./pages/HomePage";
 import { EditorPage } from "./pages/EditorPage";
@@ -14,6 +14,9 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/projects/:id" element={<ProjectDetailPage />} />
           <Route path="/editor/:modelId/:simulationId?" element={<EditorPage />} />
+
+          {/* Not Found */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </ReduxProvider>
