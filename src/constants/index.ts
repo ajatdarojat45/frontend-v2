5f3cd22280c409602abbae157303cf94e20b3365
Ajat Darojat <ajatdarojat45@gmail.com>
2025-10-04 11:38:33 +0700

    feat: add simulation methods with relevant links and documentation

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,3 +5,41 @@ export const COLORS = {
   DARK: "#1a1d29",
   GRAY: "#64748b",
 } as const;
+
+export const SIMULATION_METHODS = {
+  DE: {
+    name: "Differential Equations",
+    github: "https://building-acoustics-tu-eindhoven.github.io/acousticDE/index.html",
+    documentation: "https://building-acoustics-tu-eindhoven.github.io/acousticDE/index.html",
+  },
+  DG: {
+    name: "Discontinuous Galerkin",
+    github: "https://dg-roomacoustics.readthedocs.io/en/latest/",
+    documentation: "https://dg-roomacoustics.readthedocs.io/en/latest/",
+  },
+  PRA: {
+    name: "Pyroomacoustics",
+    github: "https://pyroomacoustics.readthedocs.io/en/pypi-release/",
+    documentation: "https://pyroomacoustics.readthedocs.io/en/pypi-release/",
+  },
+  DEISM: {
+    name: "Diffraction Enhanced Image Source Method",
+    github: "https://deism.readthedocs.io/en/latest/",
+    documentation: "https://deism.readthedocs.io/en/latest/",
+  },
+  SPPS: {
+    name: "SPPS",
+    github: "https://i-simpa.univ-gustave-eiffel.fr/",
+    documentation: "https://i-simpa.univ-gustave-eiffel.fr/",
+  },
+  DON: {
+    name: "DeepONet",
+    github: "https://github.com/dtu-act/deeponet-acoustic-wave-prop",
+    documentation: "https://github.com/dtu-act/deeponet-acoustic-wave-prop",
+  },
+  PFDTD: {
+    name: "ParallelFDTD",
+    github: "https://github.com/AaltoRSE/ParallelFDTD/",
+    documentation: "https://github.com/AaltoRSE/ParallelFDTD/",
+  },
+} as const;
