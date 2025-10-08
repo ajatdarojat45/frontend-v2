6622effa30c5f64483ec337b7ce27e0cd9d64aa7
Ajat Darojat <ajatdarojat45@gmail.com>
2025-10-08 22:55:53 +0700

    Revert "Feature/material editor"

--- src/helpers/datetime.ts
+++
@@ -1,9 +0,0 @@
-export function formatDate(dateString: string) {
-  const options: Intl.DateTimeFormatOptions = {
-    month: "short",
-    day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-  };
-  return new Date(dateString).toLocaleDateString("en-US", options);
-}
