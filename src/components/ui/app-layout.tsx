f73e327243f5be969cce72e8ef842450061f611f
Fachri Hawari <fachri.hawari@gmail.com>
2025-10-13 22:33:36 +0700

    fix: adjust header layout and improve link structure in AppLayout component

--- src/components/ui/app-layout.tsx
+++ src/components/ui/app-layout.tsx
@@ -14,15 +14,16 @@ export function AppLayout({ title, right, sidebar, children }: AppLayoutProps) {
     <div className="min-h-screen flex flex-col">
       {/* Header */}
       <header className="h-16 bg-black flex justify-between items-center">
-        <Link to="/" className="w-sidebar h-16 pl-6 border-b border-b-stone-600">
-          <h1 className="font-choras uppercase text-white text-[2.5rem] h-16 flex items-center">
-            CHORAS
-          </h1>
+        <Link
+          to="/"
+          className="w-sidebar h-16 pl-6 flex items-center border-b border-b-stone-600 font-choras uppercase text-white text-[2.5rem]"
+        >
+          CHORAS
         </Link>
         <h1 className="text-center font-choras text-choras-primary text-2xl flex-2 font-bold">
           {title}
         </h1>
-        <div className="w-sidebar shrink-0 flex-1 flex justify-end pr-6">{right}</div>
+        <div className="w-sidebar flex-1 flex justify-end pr-6">{right}</div>
       </header>
       <main className="flex flex-1">
         <aside className="w-sidebar bg-black/95 z-40">{sidebar}</aside>
